e15678147ede1d221abd323239a21c0931a759ac
visarut4998 <5835512092@psu.ac.th>
2020-03-19 20:02:22 +0700

    first

---
+++ src/config.js
@@ -0,0 +1,13 @@
+
+const config = {
+    apiKey: "AIzaSyCNIatj5NSMPJzqMjT_5iLN6_7LEEtRGdg",
+    authDomain: "todo-30579.firebaseapp.com",
+    databaseURL: "https://todo-30579.firebaseio.com",
+    projectId: "todo-30579",
+    storageBucket: "todo-30579.appspot.com",
+    messagingSenderId: "324540390464",
+    appId: "1:324540390464:web:f0487b83b80f8c4ee9d3a5",
+    measurementId: "G-H9GK10LNLL"
+  };
+
+  export default config;
